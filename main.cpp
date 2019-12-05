17676095d629fbfb222d1d62e84a00d538763160
Alec Mehra <43073945+SeaKoala@users.noreply.github.com>
2019-12-04 22:37:57 -0600

    Chunks

--- main.cpp
+++ main.cpp
@@ -5,18 +5,58 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <fstream>
 
+
+using namespace std;
+
+int parse(string filename, int n, int start);
+int getWordCount(string filename);
 
 int main(int argc, char *argv[]) {
-    char fName[] = "";
-    strcpy(fName, argv[1]);
+    char fName[] = "catchmeifyoucan.txt";
+//    strcpy(fName, argv[1]);
     printf("fileName: %s\n", fName);
-    const int n  = atoi(argv[2]); //set to a smaller number for debugging
-    printf("%d\n", n);
-
-    populateWorld(fName, world, &numRows, &numCols);
-
+//    const int n  = atoi(argv[2]); //set to a smaller number for debugging
+//    printf("%d\n", n);
+int wordCount = getWordCount(fName);
+int n = 5;
+    for(int i=0; i< wordCount-n+1; i++) {
+        parse(fName, n, i);
+    }
     return 0;
 }
 
 
+int parse(string filename, int n, int start)
+{
+    int count =0;
+    fstream file;
+    string phrase, word;
+    file.open(filename.c_str());
+        while (file >> word)
+        {
+            if(start> count){
+                count++;
+                continue;
+            }
+            phrase += word;
+            count++;
+            if((count-start)%n == 0){
+                cout << phrase<<endl;
+                break;
+            }
+        }
+    return 0;
+}
+
+int getWordCount(string filename){
+    int count =0;
+    fstream file;
+    string word;
+    file.open(filename.c_str());
+    while (file >> word) {
+        count++;
+    }
+    return count;
+}
